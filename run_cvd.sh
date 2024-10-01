01910d9bf9cc6852e4395c2a4f0df9395adc2c2c
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-01 09:12:35 -0700

    no err

--- run_cvd.sh
+++ run_cvd.sh
@@ -1,14 +1,12 @@
 set -x
-set -euo pipefail
-
 trap "pkill qemu-system; pkill run_cvd" EXIT
 
 echo parallelism: $SMP
 
 HOME=$(pwd) ./bin/launch_cvd -vm_manager qemu_cli -report_anonymous_usage_stats=n --start_webrtc=false -qemu_binary_dir=$(pwd)/.. &
 sleep 5
-(timeout 60m tail -f -n+0 cuttlefish/instances/cvd-1/logs/kernel.log |& grep -q VIRTUAL_DEVICE_BOOT_STARTED) || true
-pkill qemu-system || true
+timeout 60m tail -f -n+0 cuttlefish/instances/cvd-1/logs/kernel.log |& grep -q VIRTUAL_DEVICE_BOOT_STARTED
+pkill qemu-system
 sleep 1
-pkill run_cvd || true
+pkill run_cvd
 sleep 1
