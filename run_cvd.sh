2d117069cf7d74b4a90b2eb184e6cfe5f1f08054
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-09-30 20:11:15 -0700

    increase timeout

--- run_cvd.sh
+++ run_cvd.sh
@@ -7,7 +7,7 @@ echo parallelism: $SMP
 
 (HOME=$(pwd) ./bin/launch_cvd -vm_manager qemu_cli -report_anonymous_usage_stats=n --start_webrtc=false -qemu_binary_dir=$(pwd)/.. |& tee out.log)&
 sleep 2
-(timeout 60m tail -f -n+0 out.log  |& grep -q VIRTUAL_DEVICE_BOOT_STARTED) || true
+(timeout 120m tail -f -n+0 out.log  |& grep -q VIRTUAL_DEVICE_BOOT_STARTED) || true
 pkill qemu-system || true
 sleep 1
 pkill run_cvd || true
