35be6dd7fcd181145180c58e8597cea76d3a9858
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-01 09:03:55 -0700

    BOOT_STARTED

--- run_cvd.sh
+++ run_cvd.sh
@@ -7,7 +7,7 @@ echo parallelism: $SMP
 
 HOME=$(pwd) ./bin/launch_cvd -vm_manager qemu_cli -report_anonymous_usage_stats=n --start_webrtc=false -qemu_binary_dir=$(pwd)/.. &
 sleep 5
-(timeout 60m tail -f -n+0 cuttlefish/instances/cvd-1/logs/kernel.log |& grep -q VIRTUAL_DEVICE_BOOT_COMPLETED) || true
+(timeout 60m tail -f -n+0 cuttlefish/instances/cvd-1/logs/kernel.log |& grep -q VIRTUAL_DEVICE_BOOT_STARTED) || true
 pkill qemu-system || true
 sleep 1
 pkill run_cvd || true
