28c5cdef14a3c7b41ed38da89e4b761a18c99d14
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-01 08:53:17 -0700

    tail kernel.log

--- run_cvd.sh
+++ run_cvd.sh
@@ -5,9 +5,9 @@ trap "pkill qemu-system; pkill run_cvd" EXIT
 
 echo parallelism: $SMP
 
-(HOME=$(pwd) ./bin/launch_cvd -vm_manager qemu_cli -report_anonymous_usage_stats=n --start_webrtc=false -qemu_binary_dir=$(pwd)/.. |& tee out.log)&
-sleep 2
-(timeout 120m tail -f -n+0 out.log  |& grep -q VIRTUAL_DEVICE_BOOT_STARTED) || true
+HOME=$(pwd) ./bin/launch_cvd -vm_manager qemu_cli -report_anonymous_usage_stats=n --start_webrtc=false -qemu_binary_dir=$(pwd)/.. &
+sleep 5
+(timeout 60m tail -f -n+0 cuttlefish/instances/cvd-1/logs/kernel.log |& grep -q VIRTUAL_DEVICE_BOOT_COMPLETED) || true
 pkill qemu-system || true
 sleep 1
 pkill run_cvd || true
