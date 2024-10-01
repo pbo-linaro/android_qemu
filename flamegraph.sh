5e10aaa74ef33013029bbdcdc730abd0caba8215
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-09-30 18:15:15 -0700

    up

--- flamegraph.sh
+++ flamegraph.sh
@@ -6,4 +6,5 @@ data=$1
 
 export PATH=$PATH:$(pwd)/FlameGraph
 perf script -i $data | stackcollapse-perf.pl | flamegraph.pl > $data.svg
-echo "see with: firefox $data.svg"
+echo "see with:"
+echo "firefox $data.svg"
