d43638b84b59f7d2263427bf66283c7783ca10ab
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-09-30 18:13:31 -0700

    up

--- flamegraph.sh
+++ flamegraph.sh
@@ -6,4 +6,4 @@ set -x
 data=$1
 
 export PATH=$PATH:$(pwd)/FlameGraph
-perf script -i $data | ./stackcollapse-perf.pl | ./flamegraph.pl > $data.svg
+perf script -i $data | stackcollapse-perf.pl | flamegraph.pl > $data.svg
