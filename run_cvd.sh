ec9ba49aa593129ab04c89aaf338a3669b80bb4f
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-01 17:53:30 -0700

    wip

--- run_cvd.sh
+++ run_cvd.sh
@@ -4,7 +4,7 @@ trap "pkill qemu-system; pkill run_cvd" EXIT
 echo parallelism: $SMP
 
 HOME=$(pwd) ./bin/launch_cvd -vm_manager qemu_cli -report_anonymous_usage_stats=n --start_webrtc=false -qemu_binary_dir=$(pwd)/.. &
-sleep 5
+sleep 15
 timeout 60m tail -f -n+0 cuttlefish/instances/cvd-1/logs/kernel.log |& grep -q VIRTUAL_DEVICE_BOOT_STARTED
 pkill qemu-system
 sleep 1
