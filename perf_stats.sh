3cd3df536e0d63b4b6487f9ec9ada43fe2b4c0b5
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-02 16:07:56 -0700

    stats

--- perf_stats.sh
+++ perf_stats.sh
@@ -3,5 +3,9 @@
 set -euo pipefail
 
 data=$1
-perf report -i $data --stdio --header-only | grep duration
+time_ms=$(perf report -i $data --stdio --header-only | grep duration | sed -e 's/.*: //' -e 's/\..*//')
+time_s=$((time_ms / 1000))
+min=$((time_s / 60))
+sec=$((time_s % 60))
+echo "${min}m, ${sec}s (total ${time_s}s)"
 perf report -i $data -g none --stdio | grep -v '^#' | head -n 30
