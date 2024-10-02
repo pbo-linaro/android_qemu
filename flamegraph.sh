421647c484d706262ebd84b03980d26d84d6516b
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-01 17:42:22 -0700

    qemu local

--- flamegraph.sh
+++ flamegraph.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 data=$1
 
-export PATH=$PATH:$(pwd)/FlameGraph
+script_dir=$(dirname $(readlink -f $0))
+export PATH=$PATH:$script_dir/FlameGraph
 perf script -i $data | stackcollapse-perf.pl | flamegraph.pl > $data.svg
 echo "see with:"
 echo "firefox $data.svg"
--- perf_stats.sh
+++ perf_stats.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 data=$1
-erf report -i $data -g none --stdio | grep -v '^#' | head -n 30
+perf report -i $data -g none --stdio | grep -v '^#' | head -n 30
